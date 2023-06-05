6de3a96801d035f659acc04c7ab8d2e4d6a516bb
Mandy Petrakis <mandypetrakis@gmail.com>
2023-06-05 13:47:03 -0400

    commit tree 402f9c6bc54ac64ad47f59914e0b46b356619fa7

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,12 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = Student.all.find_by(first_name: params[:key1], last_name: params[:key2])
     render json: students
+  end
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
